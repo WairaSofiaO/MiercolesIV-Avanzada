0bbf92a5d5e8dcf23b678101066b60634e16c41e
sofia.ocampo <sofia.ocampo@accenture.com>
2020-04-22 22:08:56 -0500

    modulo de operaciones matematicas

---
+++ calculadora/operaciones.js
@@ -0,0 +1,20 @@
+module.exports={
+    sumar: function(a,b){
+         return a+b;
+    },
+    restar: function(a,b){
+        return a-b;
+    },
+    mult: function(a,b){
+         return a*b;
+    },
+    divi: function(a,b){
+        return a/b;
+    }
+}
+module.exports={
+    sumar,
+    restar,
+    mult,
+    divi
+}
